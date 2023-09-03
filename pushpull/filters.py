995d24a2d096284005cac99ee90cea568bc65b47
Matthew McAllister <matthew.mcallister.0@gmail.com>
2023-09-02 17:14:51 -0700

    Fix timezone display in local dev

--- pushpull/filters.py
+++ pushpull/filters.py
@@ -1,10 +1,14 @@
+from datetime import datetime
+
 import pytz
+from flask import Flask
 
 
-def utc_to_local(value, format):
+def utc_to_local(value: datetime, format: str):
+    value = pytz.utc.localize(value)
     new = value.astimezone(pytz.timezone('US/Pacific'))
     return new.strftime(format)
 
 
-def register_filters(app):
+def register_filters(app: Flask):
     app.jinja_env.filters['utc_to_local'] = utc_to_local
